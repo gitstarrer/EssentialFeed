8ffc79d205c3601f6e25dccd229f4ff41e50f21a
Himanshu Gupta <h23himanshu@gmail.com>
2025-07-28 12:07:38 +0530

    Initial Commit

---
+++ EssentialFeed/EssentialFeed.swift
@@ -0,0 +1,9 @@
+//
+//  EssentialFeed.swift
+//  EssentialFeed
+//
+//  Created by Himanshu on 28/07/25.
+//
+
+import Foundation
+
---
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedTests.swift
+//  EssentialFeedTests
+//
+//  Created by Himanshu on 28/07/25.
+//
+
+import Testing
+@testable import EssentialFeed
+
+struct EssentialFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
