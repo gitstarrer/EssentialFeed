9cdedc9ed8a3316c0300a6f553d5fb16398bcfa5
Himanshu Gupta <h23himanshu@gmail.com>
2025-07-28 16:35:47 +0530

    wrap the result into a client result type

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -71,23 +71,22 @@ struct RemoteFeedLoaderTests {
     }
     
     private class HttpClientSpy: HTTPClient {
-        var messages = [(urls: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
+        var messages = [(urls: URL, completion: (HTTPClientResult) -> Void)]()
         var requestedURLs: [URL] {
             messages.map(\.urls)
         }
         
-        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
             messages.append((url, completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error, nil)
+            messages[index].completion(.failure(error))
         }
         
         func complete(with statusCode: Int = 200, at index: Int = 0) {
-            let response = HTTPURLResponse(url: requestedURLs[index], statusCode: statusCode, httpVersion: nil, headerFields: nil)
-            messages[index].completion(nil, response)
+            let response = HTTPURLResponse(url: requestedURLs[index], statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+            messages[index].completion(.success(response))
         }
-        
     }
 }
