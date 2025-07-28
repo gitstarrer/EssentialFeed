479ccf8d9fbd6b3a490c6ed954015b254c488979
Himanshu Gupta <h23himanshu@gmail.com>
2025-07-28 12:11:26 +0530

    create the feed item model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Himanshu on 28/07/25.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: String
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
