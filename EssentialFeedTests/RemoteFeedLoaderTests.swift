1e26586a5cf95898c78f0c57e64dd9fb33f0f582
Himanshu Gupta <h23himanshu@gmail.com>
2025-07-28 14:41:09 +0530

    improve readability with helper method

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -45,7 +45,7 @@ struct RemoteFeedLoaderTests {
         
         sut.load { capturedError.append($0) }
         let clientError = NSError(domain: "test", code: 0)
-        client.completions[0](clientError)
+        client.complete(with: clientError)
         
         #expect(capturedError == [.connectivity])
     }
@@ -63,6 +63,10 @@ struct RemoteFeedLoaderTests {
         func get(from url: URL, completion: @escaping (Error) -> Void) {
             completions.append(completion)
             requestedURLs.append(url)
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            completions[index](error)
         }
         
     }
