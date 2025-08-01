fa019b1f412c14f5764bd307403bb0016cca1b46
Himanshu Gupta <h23himanshu@gmail.com>
2025-08-01 13:32:50 +0530

    use explicit JSON data for non 200 response tests

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -38,8 +38,8 @@ public final class RemoteFeedLoader {
     public func load(completion: @escaping (RemoteFeedLoader.Result) -> Void) {
         client.get(from: url, completion: { result in
             switch result {
-            case let .success(data, _):
-                if let root = try? JSONDecoder().decode(Root.self, from: data) {
+            case let .success(data, resposne):
+                if resposne.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
                     completion(.success(root.items))
                 } else {
                     completion(.failure(.invalidData))
--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -53,7 +53,8 @@ struct RemoteFeedLoaderTests {
         let samples = [199, 201, 300, 400, 501]
         samples.enumerated().forEach { index, code in
             expect(sut, toCompleteWith: .failure(.invalidData)) {
-                client.complete(withStatusCode: code, at: index)
+                let json = makeItemsJSON([])
+                client.complete(withStatusCode: code, data: json, at: index)
             }
         }
     }
@@ -139,7 +140,7 @@ struct RemoteFeedLoaderTests {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int = 200, data: Data = Data(), at index: Int = 0) {
+        func complete(withStatusCode code: Int = 200, data: Data, at index: Int = 0) {
             let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)!
             messages[index].completion(.success(data, response))
         }
