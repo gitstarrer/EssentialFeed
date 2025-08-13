081a63d2b698021a712d945db123766d179eca42
Himanshu Gupta <h23himanshu@gmail.com>
2025-08-13 19:29:58 +0530

    add CI scheme

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -30,6 +30,7 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
         }
     }
 
+    //Helpers: 
     private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> LoadFeedResult? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient()
